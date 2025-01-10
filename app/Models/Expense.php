1f409bb43a7e7d651e5465b0b43be86d18ce7073
carobte <caro.bustamante.escobar@gmail.com>
2025-01-10 10:23:33 -0500

    feat: categories and expenses tables

---
+++ app/Models/Expense.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'amount',
+        'date',
+        'category_id',
+    ];
+
+    // Category relation
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Validaciones
+    public static $rules = [
+        'description' => 'required|max:255',
+        'amount' => 'required|integer|min:1',
+        'date' => 'required|date',
+        'category_id' => 'required|exists:categories,id',
+    ];
+}
